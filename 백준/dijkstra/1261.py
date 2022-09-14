b7680f03509c6e1de73b317fb5ef7e9517c1f9f3
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-09-14 11:35:18 +0900

    dijkstra

---
+++ 백준/dijkstra/1261.py
@@ -0,0 +1,32 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+a = []
+
+for i in range(m):
+    a.append(input())
+
+dist = [[-1] * n for _ in range(m)]
+dist[0][0] = 0
+q = deque()
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+q.append([0, 0])
+
+while q:
+    x, y = q.popleft()
+    for i in range(4):
+        nx, ny = x + dx[i], y + dy[i]
+        if 0 <= nx < m and ny >= 0 and ny < n:
+            if dist[nx][ny] == -1:
+                if a[nx][ny] == "0":
+                    q.appendleft([nx, ny])
+                    dist[nx][ny] = dist[x][y]
+                elif a[nx][ny] == "1":
+                    q.append([nx, ny])
+                    dist[nx][ny] = dist[x][y] + 1
+
+print(dist[m - 1][n - 1])
---
+++ 백준/dijkstra/1504.py
@@ -0,0 +1,41 @@
+import heapq
+import sys
+
+input = sys.stdin.readline
+
+n, e = map(int, input().split())
+graph = [[] for i in range(n + 1)]
+INF = int(1e9)
+
+for _ in range(e):
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
+    graph[b].append((a, c))
+
+v1, v2 = map(int, input().split())
+
+
+def dijkstra(start):
+    q = []
+    distance = [INF] * (n + 1)
+    heapq.heappush(q, (0, start))
+    distance[start] = 0
+
+    while q:
+        dist, now = heapq.heappop(q)
+        if distance[now] < dist:
+            continue
+        for i in graph[now]:
+            cost = dist + i[1]
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+
+    return distance
+
+
+one = dijkstra(1)
+v1_ = dijkstra(v1)
+v2_ = dijkstra(v2)
+cnt = min(one[v1] + v1_[v2] + v2_[n], one[v2] + v2_[v1] + v1_[n])
+print(cnt if cnt < INF else -1)
