8451c9c1fe716ae82cd8d2ccce10e570a2fa370a
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-09-18 00:00:43 +0900

    bfs

--- 이것이코딩테스트다/dfs&bfs/음료수얼려먹기.py
+++ 이것이코딩테스트다/dfs&bfs/음료수얼려먹기.py
@@ -1,35 +1,38 @@
-# n, m 입력 받기
-n, m = list(map(int, input().split()))
+from collections import deque
 
-# 2차원 리스트의 맵 정보 입력받기
+n, m = map(int, input().split())
 graph = []
 for i in range(n):
     graph.append(list(map(int, input())))
 
-# DFS로 특정한 노드를 방문한 뒤에 연결된 모든 노드들도 방문
-def dfs(x, y):
-    # 주어진 범위를 벗어나는 경우에는 즉시 종료
-    if x <= -1 or x >= n or y <= -1 or y >= m:
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
+
+
+def bfs(x, y):
+    queue = deque()
+    queue.append((x, y))
+
+    if graph[x][y] == 1:
         return False
-    # 현재 노드를 아직 방문하지 않았다면
-    if graph[x][y] == 0:
-        # 현재 노드 방문 처리
+
+    while queue:
+        x, y = queue.popleft()
         graph[x][y] = 1
-        # 상, 하, 좌, 우의 위치도 모두 재귀적으로 호출
-        dfs(x, y + 1)
-        dfs(x, y - 1)
-        dfs(x - 1, y)
-        dfs(x + 1, y)
-        return True
-    return False
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 0:
+                queue.append((nx, ny))
+
+    return True
 
 
-# 모든 노드에 대하여 음료수 채우기
-result = 0
+cnt = 0
+
 for i in range(n):
     for j in range(m):
-        # 현재 위치에서 DFS 수행
-        if dfs(i, j) == True:
-            result += 1
+        if bfs(i, j):
+            cnt += 1
 
-print(result)
+print(cnt)
