d06a4081519ff789307b909dfbfb7eb044c3ad37
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-10-01 11:10:48 +0900

    implementation

--- 백준/implementation/9251.py
+++ 백준/implementation/9251.py
@@ -3,7 +3,6 @@ S2 = list(input())
 len1 = len(S1)
 len2 = len(S2)
 dp = [[0] * (len2 + 1) for _ in range(len1 + 1)]
-print(dp)
 
 for i in range(1, len1 + 1):
     for j in range(1, len2 + 1):
@@ -11,7 +10,5 @@ for i in range(1, len1 + 1):
             dp[i][j] = dp[i - 1][j - 1] + 1
         else:
             dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
-
-print(dp)
 
 print(dp[len1][len2])
