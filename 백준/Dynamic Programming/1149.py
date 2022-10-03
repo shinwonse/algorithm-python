d5608a589fa8c7f7579360e0ff52238c29251fb9
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-10-03 15:56:59 +0900

    dp

--- 백준/Dynamic Programming/1149.py
+++ 백준/Dynamic Programming/1149.py
@@ -1,3 +1,6 @@
+# i번째 집을 1번째 색으로 칠하는 최소 비용의 경우의 수는 i - 1번째 집을 2번째색, 3번째색 으로 칠했을 때의
+# 최소 비용에 i번째 집의 1번째 색 비용을 더한 값이다.
+
 import sys
 
 input = sys.stdin.readline
@@ -9,6 +12,8 @@ for i in range(N):
     dp.append(list(map(int, input().split())))
 
 for i in range(1, len(dp)):
-    dp[i][0] += min(dp[i - 1][1], dp[i - 2][2])
+    dp[i][0] += min(dp[i - 1][1], dp[i - 1][2])
     dp[i][1] += min(dp[i - 1][0], dp[i - 1][2])
     dp[i][2] += min(dp[i - 1][1], dp[i - 1][0])
+
+print(min(dp[N - 1]))
