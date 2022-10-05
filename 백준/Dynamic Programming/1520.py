e716205b0b5b8b4f2cc3cd88cd0683a98b4b73f3
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-10-05 23:16:15 +0900

    dp

--- 백준/Dynamic Programming/1520.py
+++ 백준/Dynamic Programming/1520.py
@@ -23,14 +23,14 @@ def dfs(x, y):
         nx, ny = x + dx[i], y + dy[i]
 
         if 0 <= nx <= M - 1 and 0 <= ny <= N - 1:
-            if nums[x][y] > nums[nx][ny]:
+            if graph[x][y] > graph[nx][ny]:
                 dp[x][y] += dfs(nx, ny)
 
     return dp[x][y]
 
 
 M, N = map(int, input().split())
-nums = [list(map(int, input().split())) for _ in range(M)]
+graph = [list(map(int, input().split())) for _ in range(M)]
 
 dp = [[-1] * N for _ in range(M)]
 print(dfs(0, 0))
