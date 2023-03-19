b7a2c68b6094238d321712c0ed7a2786cd8324ce
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2023-03-19 20:19:19 +0900

    음료수 얼려먹기

--- 이것이코딩테스트다/dfs&bfs/음료수얼려먹기.py
+++ 이것이코딩테스트다/dfs&bfs/음료수얼려먹기.py
@@ -1,38 +1,29 @@
 from collections import deque
 
 n, m = map(int, input().split())
+
 graph = []
 for i in range(n):
     graph.append(list(map(int, input())))
 
-dx = [0, 0, 1, -1]
-dy = [1, -1, 0, 0]
 
-
-def bfs(x, y):
-    queue = deque()
-    queue.append((x, y))
-
-    if graph[x][y] == 1:
+def dfs(x, y):
+    if x <= -1 or x >= n or y <= -1 or y >= m:
         return False
-
-    while queue:
-        x, y = queue.popleft()
+    if graph[x][y] == 0:
         graph[x][y] = 1
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < n and 0 <= ny < m and graph[nx][ny] == 0:
-                queue.append((nx, ny))
+        dfs(x - 1, y)
+        dfs(x, y - 1)
+        dfs(x + 1, y)
+        dfs(x, y + 1)
+        return True
+    return False
 
-    return True
 
-
-cnt = 0
-
+result = 0
 for i in range(n):
     for j in range(m):
-        if bfs(i, j):
-            cnt += 1
+        if dfs(i, j):
+            result += 1
 
-print(cnt)
+print(result)
